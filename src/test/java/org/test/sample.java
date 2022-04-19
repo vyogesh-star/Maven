485fa1a73447dece689a90cf7c3a1c349bd0bc97
vyogesh-star <vyogesh13@gmail.com>
2022-04-19 22:39:40 +0530

    Project Structure

---
+++ src/test/java/org/test/sample.java
@@ -0,0 +1,14 @@
+package org.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class sample {
+
+
+}
